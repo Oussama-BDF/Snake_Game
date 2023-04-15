7c9bfcc4125c53ef9ed85cf19924f5a8e134235a
OussamaBdf <oussama.boudafdafa@gmail.com>
2023-04-15 18:10:18 +0000

    Run the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <SFML/Graphics.hpp>
+#include "GameLogic.hpp"
 
 int main(){
 
     sf::RenderWindow window(sf::VideoMode(816, 624), "Snake Game");
+    GameLogic game1(window.getSize());
+    game1.startPlay(window);
     return 0;
 }
